0477427c4ece4ac92ad66b341e6d1739ede4ba41
Ting A Lin <linshaoting6@gmail.com>
2024-08-20 14:54:13 +0100

    add db

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # proxy-caller
+
+### Build and run
+
+```bash
+cargo build --locked --release -p proxy-caller
+
+# start sync
+./target/release/proxy-caller
+
+```
--- sync/src/main.rs
+++ sync/src/main.rs
@@ -2,11 +2,21 @@ use candid::{Decode, Encode, Nat};
 use ic_agent::{
 	agent::http_transport::ReqwestTransport, export::Principal, identity::Secp256k1Identity, Agent,
 };
-use std::error::Error;
 use icrc_ledger_types::icrc3::transactions::{GetTransactionsRequest, GetTransactionsResponse};
+use anyhow::anyhow;
+use dotenvy::dotenv;
+use utils::Database;
+mod utils;
+
 
 #[tokio::main]
-pub async fn main() -> Result<(), Box<dyn Error>> {
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+	dotenv().ok();
+
+	let db_url = std::env::var("DATABASE_URL").map_err(|_| anyhow!("DATABASE_URL is not found"))?;
+	let db = Database::new(db_url.clone()).await;
+
+
 	let network = "https://ic0.app".to_string();
 
 	let agent_identity = Secp256k1Identity::from_pem(
@@ -25,7 +35,22 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
 
 	let canister_id = Principal::from_text("mxzaz-hqaaa-aaaar-qaada-cai".to_string())?;
 
-	let reqst = GetTransactionsRequest{start: Nat::from(1_733_000u64), length: Nat::from(4u64)};
+	// let start_index = match{
+	// 	// start_index: read from the db
+	// // if none, read from the current.
+
+	// // let init_reqst = GetTransactionsRequest{start: Nat::from(0u64), length: Nat::from(0u64)};
+	// // let init_arg = Encode!(&init_reqst)?;
+	// // let init_ret = agent
+	// // .update(&canister_id, "get_transactions")
+	// // .with_arg(init_arg)
+	// // .call_and_wait()
+	// // .await?;
+	// // let init_answer = Decode!(&init_ret, GetTransactionsResponse)?;
+	// // let start_index  = init_answer.first_index;
+	// };
+
+	let reqst = GetTransactionsRequest{start: start_index, length: Nat::from(2u64)};
 	let arg = Encode!(&reqst)?;
 
 	let ret = agent
@@ -35,8 +60,9 @@ pub async fn main() -> Result<(), Box<dyn Error>> {
 	.await?;
 
 	let answer = Decode!(&ret, GetTransactionsResponse)?;
+	// 只要get_transactions成功了就可以更新DB的block index了
 
-	let proxy_account = vec![Principal::from_text("lrf2i-zba54-pygwt-tbi75-zvlz4-7gfhh-ylcrq-2zh73-6brgn-45jy5-cae".to_string())?, Principal::from_text("q2u76-ayx43-jvcbk-klmdu-6akxa-olhvs-mkjqf-td2ok-xi7tl-megex-kqe".to_string())?];
+	let proxy_account = vec![Principal::from_text("il25e-7ncru-p5jb5-zu6tn-wjetc-nmh5d-4aplx-qre2t-ww6gy-ahtzz-yae".to_string())?, Principal::from_text("ix5qj-xyaaa-aaaar-qahfa-cai".to_string())?];
 
 	for acc in proxy_account {
 		for tx in answer.clone().transactions {
